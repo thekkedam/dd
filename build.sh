7ae04c90cc66230b7095d8f4ea100ee4e4bcc6dd
Vipin Madhavanunni <vipmadha@gmail.com>
2015-12-02 16:03:00 +0100

    Fixing sitemap time

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ rm -rf .sass-cache
 set -e
 set -x
 
-sed -i "s/^date:.*$/date: $(TZ=UTC date "+%Y-%m-%d %H:%M:%S %Z")/" _config.yml
+sed -i "s/^date:.*$/date: $(date -u +%Y-%m-%dT%H:%M:%S%z)/" _config.yml
 git add _config.yml
 
 bundle exec jekyll serve --trace
