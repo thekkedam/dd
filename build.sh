59ca33a1c0b414b14ba0a3ea1278609374513464
Vipin Madhavanunni <vipmadha@gmail.com>
2015-12-02 15:46:04 +0100

    Adding support for google site map

--- build.sh
+++ build.sh
@@ -3,4 +3,10 @@
 rm -rf _site
 rm -rf .sass-cache
 
+set -e
+set -x
+
+sed -i "s/^date:.*$/date: $(TZ=UTC date "+%Y-%m-%d %H:%M:%S %Z")/" _config.yml
+git add _config.yml
+
 bundle exec jekyll serve --trace
