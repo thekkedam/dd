fc8bb83e4d07c014b8f45a13ffdfe466e92fb740
Vipin Madhavanunni <vipmadha@gmail.com>
2015-12-06 11:53:34 +0100

    Updating date format in google site index

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 rm -rf _site
-rm -rf .sass-cache
 
-set -e
-set -x
-
-sed -i "s/^date:.*$/date: $(date -u +%Y-%m-%dT%H:%M:%S%z)/" _config.yml
 git add _config.yml
 
-bundle exec jekyll serve --trace
+bundle exec jekyll serve --trace $@
