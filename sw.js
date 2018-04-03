0aa4b40d5caac86b7337cd0ef7fe7f4c9488a7b5
Vipin Madhavanunni <vipintm@gmail.com>
2018-04-03 02:52:42 +0200

    Updated-04-03-2018-02:52

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 var APP_PREFIX = 'DeepthiAkkoorath_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'Web_00002'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'Web_00003'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            // Add URL you want to cache in this list.
   '/assets/css/',                     // If you have separate JS/CSS files,
