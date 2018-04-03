22b73637049ff753695f1ba5fd41b6eda2d42604
Vipin Madhavanunni <vipintm@gmail.com>
2018-04-03 21:50:32 +0200

    Updated-04-03-2018-21:49

--- sw.js
+++ sw.js
@@ -1,69 +1,172 @@
-var APP_PREFIX = 'DeepthiAkkoorath_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'Web_00003'              // Version of the off-line cache (change this value everytime you want to update cache)
-var CACHE_NAME = APP_PREFIX + VERSION
-var URLS = [                            // Add URL you want to cache in this list.
-  '/assets/css/',                     // If you have separate JS/CSS files,
-  '/assets/js/',
-  '/index.html',            // add path to those files here
-  '/404.html',
-  '/cms.html',
-  '/projects/',
-  '/publications/',
-  '/talks/',
-  '/research/',
-  '/cv/',
-  '/contact/',
-  '/about-site/',
-  '/LICENSE/'
-]
 
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
+'use strict';
 
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
+const version = 'v000000001';
+const offlineResources = [
+  '/',
+  '/offline.html',
+  '/offline.svg'
+];
+
+const ignoreFetch = [
+  /https?:\/\/dd.thekkedam.org\/cms.html/,
+  /https?:\/\/dd.thekkedam.org\/files\/Akkoorath_Deepthi-Devaki_CV.pdf/,
+];
+
+const offlineImage = '<svg width="400" height="300" role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>';
+
+//////////
+// Install
+//////////
+function onInstall(event) {
+  log('install event in progress.');
+
+  event.waitUntil(updateStaticCache());
+}
+
+function updateStaticCache() {
+  return caches
+    .open(cacheKey('offline'))
+    .then((cache) => {
+      return cache.addAll(offlineResources);
     })
-  )
-})
+    .then(() => {
+      log('installation complete!');
+    });
+}
 
-// Cache resources
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(URLS)
+////////
+// Fetch
+////////
+function onFetch(event) {
+  const request = event.request;
+
+  if (shouldAlwaysFetch(request)) {
+    event.respondWith(networkedOrOffline(request));
+    return;
+  }
+
+  if (shouldFetchAndCache(request)) {
+    event.respondWith(networkedOrCached(request));
+    return;
+  }
+
+  event.respondWith(cachedOrNetworked(request));
+}
+
+function networkedOrCached(request) {
+  return networkedAndCache(request)
+    .catch(() => { return cachedOrOffline(request) });
+}
+
+// Stash response in cache as side-effect of network request
+function networkedAndCache(request) {
+  return fetch(request)
+    .then((response) => {
+      var copy = response.clone();
+      caches.open(cacheKey('resources'))
+        .then((cache) => {
+          cache.put(request, copy);
+        });
+
+      log("(network: cache write)", request.method, request.url);
+      return response;
+    });
+}
+
+function cachedOrNetworked(request) {
+  return caches.match(request)
+    .then((response) => {
+      log(response ? '(cached)' : '(network: cache miss)', request.method, request.url);
+      return response ||
+        networkedAndCache(request)
+          .catch(() => { return offlineResponse(request) });
+    });
+}
+
+function networkedOrOffline(request) {
+  return fetch(request)
+    .then((response) => {
+      log('(network)', request.method, request.url);
+      return response;
     })
-  )
-})
+    .catch(() => {
+      return offlineResponse(request);
+    });
+}
 
-// Delete outdated caches
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create white list
-      var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX)
-      })
-      // add current cache name to white list
-      cacheWhitelist.push(CACHE_NAME)
+function cachedOrOffline(request) {
+  return caches
+    .match(request)
+    .then((response) => {
+      return response || offlineResponse(request);
+    });
+}
 
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] )
-          return caches.delete(keyList[i])
-        }
-      }))
+function offlineResponse(request) {
+  log('(offline)', request.method, request.url);
+  if (request.url.match(/\.(jpg|png|gif|svg|jpeg)(\?.*)?$/)) {
+    return caches.match('/offline.svg');
+  } else {
+    return caches.match('/offline.html');
+  }
+}
+
+///////////
+// Activate
+///////////
+function onActivate(event) {
+  log('activate event in progress.');
+  event.waitUntil(removeOldCache());
+}
+
+function removeOldCache() {
+  return caches
+    .keys()
+    .then((keys) => {
+      return Promise.all( // We return a promise that settles when all outdated caches are deleted.
+        keys
+         .filter((key) => {
+           return !key.startsWith(version); // Filter by keys that don't start with the latest version prefix.
+         })
+         .map((key) => {
+           return caches.delete(key); // Return a promise that's fulfilled when each outdated cache is deleted.
+         })
+      );
     })
-  )
-})
+    .then(() => {
+      log('removeOldCache completed.');
+    });
+}
+
+function cacheKey() {
+  return [version, ...arguments].join(':');
+}
+
+function log() {
+  if (developmentMode()) {
+    console.log("SW:", ...arguments);
+  }
+}
+
+function shouldAlwaysFetch(request) {
+  return __DEVELOPMENT__ ||
+    request.method !== 'GET' ||
+      ignoreFetch.some(regex => request.url.match(regex));
+}
+
+function shouldFetchAndCache(request) {
+  return ~request.headers.get('Accept').indexOf('text/html');
+}
+
+function developmentMode() {
+  return __DEVELOPMENT__ || __DEBUG__;
+}
+
+log("Hello from ServiceWorker land!", version);
+
+self.addEventListener('install', onInstall);
+
+self.addEventListener('fetch', onFetch);
+
+self.addEventListener("activate", onActivate);
