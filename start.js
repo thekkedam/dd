ce98e7605cb024575a47849a295b0a8c882fb26d
Vipin Madhavanunni <vipintm@gmail.com>
2018-05-08 00:22:22 +0200

    Updated-05-08-2018-00:20

--- start.js
+++ start.js
@@ -9,20 +9,11 @@ UpUp.start({
       {% if section[0] == 'content-url' %}
         'content-url': '{{ section[1] }}',
       {% elsif section[0] == 'assets' %}
-        'assets': [
-        {% for bullet in section[1] %}
-          '{{ bullet }}'{% unless forloop.last %},{% endunless %}
-        {% endfor %}
-        ]
+        'assets': [{% for bullet in section[1] %} '{{ bullet }}'{% unless forloop.last %},{% endunless %}{% endfor %} ]
       {% endif %}
     {% endfor %}
   {% endfor %}
 });
-
-// load ofline css when ofline
-//if(navigator.onLine == false ) {
-//  $('head').append('<link rel="stylesheet" type="text/css" href="/assets/css/offline.css">');
-//}
 
 // https://stackoverflow.com/questions/13549997/how-to-append-all-href-link-with-redirector-url-via-javascript
 var links = document.links;
@@ -31,9 +22,14 @@ while (i--) {
   if (links[i].getAttribute("href").slice(0, 4) != "http") {
     continue;
   }
-  if (links[i].getAttribute("href").slice(-1) != "/") {
-    links[i].href = links[i].href + "&{{ site.data.config.utm }}";
-  } else {
+  if (links[i].getAttribute("href").slice(-1) == "/") {
     links[i].href = links[i].href + "{{ site.data.config.utm }}";
+  } else {
+    var href = links[i].getAttribute("href");
+    if ( href.search(/\?/) > -1) {
+      links[i].href = links[i].href + "&{{ site.data.config.utm }}";
+    } else {
+      links[i].href = links[i].href + "{{ site.data.config.utm }}";
+    }
   }
 }
