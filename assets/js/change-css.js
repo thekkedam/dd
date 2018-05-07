8d0638225ae15008bcb633c9136b9a730823e816
Vipin Madhavanunni <vipintm@gmail.com>
2018-05-08 00:22:22 +0200

    Updated-05-08-2018-00:20

---
+++ assets/js/change-css.js
@@ -0,0 +1,17 @@
+function changeCSS(cssFile, cssLinkIndex) {
+
+    var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", cssFile);
+
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+}
+
+// load ofline css when ofline
+if(navigator.onLine == false ) {
+ changeCSS('/assets/css/offline.css', 0);
+}
+
